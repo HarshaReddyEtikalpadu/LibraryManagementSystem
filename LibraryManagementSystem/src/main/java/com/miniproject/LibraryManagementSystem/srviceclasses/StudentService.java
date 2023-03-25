f1b2ecea8f1d51b881d4469cc29409cdfa890639
= <yourstrulyyrahul@gmail.com>
2023-03-25 18:13:37 +0530

    Added service layer logic to mark book as not available.

--- LibraryManagementSystem/src/main/java/com/miniproject/LibraryManagementSystem/srviceclasses/StudentService.java
+++ LibraryManagementSystem/src/main/java/com/miniproject/LibraryManagementSystem/srviceclasses/StudentService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import javax.management.RuntimeErrorException;
 
+import com.miniproject.LibraryManagementSystem.repo.BookRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
